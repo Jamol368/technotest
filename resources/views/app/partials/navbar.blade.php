a19f0fe1e2bc506411f2d3ce86c58309be45bb66
Jamol <J.Axmedov@uzinfocom.uz>
2025-12-16 21:18:37 +0500

    Removed text-white class from toggle-menu

--- resources/views/app/partials/navbar.blade.php
+++ resources/views/app/partials/navbar.blade.php
@@ -63,7 +63,7 @@
                     @endguest
                 </ul>
             </nav>
-            <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-white"><span class="icon-menu h3 text-white"></span></a></div>
+            <div class="ml-auto toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle"><span class="icon-menu h3"></span></a></div>
         </div>
     </div>
 </header>
